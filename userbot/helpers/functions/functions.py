47b75bdae1bd51d30f09e33c07cfb19beb3e280e
Iceysm <shivmunot971@gmail.com>
2024-08-12 23:14:37 +0530

    Update functions.py

--- userbot/helpers/functions/functions.py
+++ userbot/helpers/functions/functions.py
@@ -93,8 +93,8 @@ async def covidindia(state):
 async def post_to_telegraph(
     page_title,
     html_format_content,
-    auth_name="CatUserbot",
-    auth_url="https://t.me/catuserbot17",
+    auth_name="Userbot",
+    auth_url="https://t.me/rrbofficialmain",
 ):
     post_client = TelegraphPoster(use_api=True)
     post_client.create_api_token(auth_name)
